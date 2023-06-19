ccb242af16ea90968308931d6ce59a258ba4911f
Waheedullah Khan <waheedullahkhan1250@gmail.com>
2023-06-19 16:39:17 +0500

    Added jwt support in swagger

--- src/main/java/com/cloud/notesbackend/Application.java
+++ src/main/java/com/cloud/notesbackend/Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    // TODO: Swagger for secure endpoints
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/cloud/notesbackend/configs/AppConfig.java
+++ src/main/java/com/cloud/notesbackend/configs/AppConfig.java
@@ -1,5 +1,9 @@
 package com.cloud.notesbackend.configs;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,5 +15,17 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(
+                        new Components()
+                                .addSecuritySchemes("JWT", new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")))
+                .addSecurityItem(new SecurityRequirement().addList("jwt"));
     }
 }
